6a68596d34fbff136a697f067e14be0945aa0d0c
Christoph Hu <christoph.huschenhoefer@gmx.de>
2024-02-26 20:14:23 +0100

    add notifications-library

---
+++ projects/npx-notifications/src/lib/npx-notifications.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-npx-notifications',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      npx-notifications works!
+    </p>
+  `,
+  styles: ``
+})
+export class NpxNotificationsComponent {
+
+}
---
+++ projects/npx-notifications/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of npx-notifications
+ */
+
+export * from './lib/npx-notifications.service';
+export * from './lib/npx-notifications.component';
