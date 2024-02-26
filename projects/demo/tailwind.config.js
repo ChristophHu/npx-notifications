d6a1f2ae3e8e83a408466e5c12e0963f49306062
Christoph Hu <christoph.huschenhoefer@gmx.de>
2024-02-26 20:07:55 +0100

    working projectsetup

---
+++ projects/demo/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './projects/**/*.{html,ts,css,scss,sass,less,styl}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
