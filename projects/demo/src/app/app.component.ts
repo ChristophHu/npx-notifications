e5110958495b5becd2a4a761e56ea7a1db322655
Christoph Hu <christoph.huschenhoefer@gmx.de>
2024-04-15 15:55:34 +0200

    add playground

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxNotificationsComponent, NgxNotificationsService } from '../../../ngx-notifications/src/public-api';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
+    JsonPipe,
     NgxNotificationsComponent,
+    ReactiveFormsModule,
     RouterOutlet
   ],
   providers: [
@@ -19,12 +22,27 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private _notificationService: NgxNotificationsService) {
+  notificationForm: FormGroup
+  types: string[] = ['error', 'request', 'success', 'warning']
 
+  constructor(private _notificationService: NgxNotificationsService) {
+    this.notificationForm = new FormGroup({
+      type: new FormControl(''),
+      header: new FormControl(''),
+      message: new FormControl(''),
+      autoClose: new FormControl(false)
+    })
   }
 
   ngOnInit(): void {
-    this.request()
+    // this.error()
+  }
+
+
+  createNotification() {
+    this._notificationService.open(this.notificationForm.value)?.subscribe((data: any) => { 
+      if (data == true) console.log('result: ', data)
+    })
   }
 
   get() {
@@ -33,25 +51,25 @@ export class AppComponent implements OnInit {
 
   error() {
     this._notificationService.open({ type: 'error', header: 'Fehler', message: 'Es ist ein Fehler aufgetreten', autoClose: false })?.subscribe((data: any) => { 
-      if (data == true) alert('yes')
+      // if (data == true) alert('yes')
     })
   }
 
   request() {
-    this._notificationService.open({ type: 'request', header: 'Streife beendet', message: 'Möchten sie eine neue Streife vorbereiten (ja) oder die angezeigte Sreife ansehen (nein)? Dann haben Sie die Möglichkeit hier. Hier soll nun auch getestet werden ob die Notification frei skaliert.', autoClose: false })?.subscribe((data: any) => { 
-      if (data == true) alert('yes')
+    this._notificationService.open({ type: 'request', header: 'Event beendet', message: 'Möchten Sie einen neuen Event vorbereiten (ja) oder den angezeigten Event ansehen (nein)? Dann haben Sie die Möglichkeit hier. Hier soll nun auch getestet werden ob die Notification frei skaliert.', autoClose: false })?.subscribe((data: any) => { 
+      // if (data == true) alert('yes')
     })
   }
 
   success() {
     this._notificationService.open({ type: 'success', header: 'Erfolg', message: 'Erfolgreich abgeschlossen', autoClose: false })?.subscribe((data: any) => { 
-      if (data == true) alert('yes')
+      // if (data == true) alert('yes')
     })
   }
 
   warning() {
-    this._notificationService.open({ type: 'warning', header: 'Warnung', message: 'Erfolgreich abgeschlossen', autoClose: false })?.subscribe((data: any) => { 
-      if (data == true) alert('yes')
+    this._notificationService.open({ type: 'warning', header: 'Achtung', message: 'Es muss folgendes beachtet werden:', autoClose: false })?.subscribe((data: any) => { 
+      // if (data == true) alert('yes')
     })
   }
 }
